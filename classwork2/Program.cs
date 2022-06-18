eb337e58ece9a13aaa37a1f0ad5d20a64b55eb74
«novdim» <nov_dim@mail.ru>
2022-06-18 11:07:29 +0300

    add SumNumbers

---
+++ classwork2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+            void SumNumbers (int number) //метод вывода кубов чисел от 1 до N.
+            {
+                int sum = 0;
+                for (int i = 1; i <= number;  i++) // циклом проходимя по массивам pointOne, pointTwo чтобы получить сумму разностей точек в квадрате 
+                {
+                    sum = sum + i;
+                }
+                Console.Write($"{sum}");
+                
+            }
+            Console.WriteLine("Введите число:");
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
+            SumNumbers(inputNumber);
+
+            
+        }
+    }
+}
