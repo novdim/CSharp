04b9fb2de52f692b5a2ed296d5e909a12916f077
«novdim» <nov_dim@mail.ru>
2022-06-18 10:06:05 +0300

    add СubeTableForGivenNumbers and minor edits

--- homework3/Program.cs
+++ homework3/Program.cs
@@ -18,6 +18,7 @@ namespace ConsoleApp
             {
                 return Regex.IsMatch(input, "^[a-zA-Z0-9]+$"); // возврат результата в формате (true, false) проверки наличия (букв и цифр) регулярным выражением
             }
+
             Console.Write("Введите слово (число) из 5 и более букв или цифр.\n");
             string inputText = Console.ReadLine();  
             bool lettersOrNumbers = IsAlphaNumeric(inputText);
@@ -66,6 +67,10 @@ namespace ConsoleApp
 
             PalindromeCheck(inputText);
 
+            Console.Write("Проверим 2 версию определителя палиндромома. \n");
+            Console.Write("Введите слово (число) из 5 и более букв или цифр.\n");
+            string inputText_V2 = Console.ReadLine();  
+            bool lettersOrNumbers_V2 = IsAlphaNumeric(inputText);
 
             void PalindromeCheck_V2 (string text) // метод проверки палиндромома на введенные 5 и более букв или цифр (версия 2) 
             {
@@ -74,7 +79,7 @@ namespace ConsoleApp
 
                 while(true)
                 {
-                    if (chars.Length < 5 || lettersOrNumbers == false)  // проверяем на количество букв(цифр) и на то чтобы не было других символов 
+                    if (chars.Length < 5 || lettersOrNumbers_V2 == false)  // проверяем на количество букв(цифр) и на то чтобы не было других символов 
                     {
                         Console.Write("Введите слово (число) из 5 и более букв или цифр.\n");
                         text = Console.ReadLine();
@@ -99,8 +104,9 @@ namespace ConsoleApp
                     }
                 }                
             }
-            Console.Write("Проверим 2 версию определителя палиндромома. \n");
-            PalindromeCheck_V2(inputText);
+
+            PalindromeCheck_V2(inputText_V2);
+
         /*
         Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
         A (3,6,8); B (2,1,-7), -> 15.84
@@ -109,7 +115,7 @@ namespace ConsoleApp
 
         double CorrectNamber() // Метод (функция) проверки вводимого числа 
         {
-            double namber; // обьявляем переменную namber типа integer
+            double namber; // обьявляем переменную namber типа double
             while (!double.TryParse(Console.ReadLine(), out namber)) // Вводим символы. Проверяем если введенно не число выполняем цикл. Если введено число передаем его в переменную namber
             {
                 Console.WriteLine("Ошибка ввода! Введите число!");
@@ -118,14 +124,14 @@ namespace ConsoleApp
         }
 
 
-        void DistancePoints3dSpace (double[] pointOne, double[] pointTwo)
+        void DistancePoints3dSpace (double[] pointOne, double[] pointTwo) //метод определения расстояния меду точками в 3D пространстве
         {
-            double sumDifferences = 0;
-            for (int i = 0; i <= 2;  i++)
+            double sumDifferences = 0;  
+            for (int i = 0; i <= 2;  i++) // циклом проходимя по массивам pointOne, pointTwo чтобы получить сумму разностей точек в квадрате 
             {
-                sumDifferences = sumDifferences + Convert.ToDouble(Math.Pow((pointOne[i] - pointTwo[i]), 2));
+                sumDifferences = sumDifferences + Convert.ToDouble(Math.Pow((pointOne[i] - pointTwo[i]), 2)); // методом Pow возводим во 2 степень полученные разницы
             }
-            Console.WriteLine($"{Math.Sqrt(sumDifferences)} ");
+            Console.WriteLine($"{Math.Sqrt(sumDifferences)} "); // выводим корень уже полученной суммы
             
         }
         Console.WriteLine("Введите кординату X точки A");
@@ -153,7 +159,25 @@ namespace ConsoleApp
         5 -> 1, 8, 27, 64, 125
         */
 
-
+        void СubeTableForGivenNumbers (int number) //метод вывода кубов чисел от 1 до N.
+        {
+            int sumDifferences = 0;  
+            for (int i = 1; i <= number;  i++) // циклом проходимя по массивам pointOne, pointTwo чтобы получить сумму разностей точек в квадрате 
+            {
+                if (i != number) // чтобы последнее число выводилось без запятой проверяем дошел ли цикл до последнего элемента
+                {
+                    Console.Write($"{Convert.ToInt32(Math.Pow(i, 3))}, "); // методом Pow возводим во 3 степень
+                }
+                else
+                {
+                    Console.Write($"{Convert.ToInt32(Math.Pow(i, 3))}"); // методом Pow возводим во 3 степень
+                }
+            }
+            
+        }
+        Console.WriteLine("Введите число:");
+        int inputNumber = Convert.ToInt32(Console.ReadLine());
+        СubeTableForGivenNumbers(inputNumber);
         }
     }
 }
