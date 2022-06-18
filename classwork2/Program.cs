48a49e5473f177e41de93ade05466cda54594e4d
«novdim» <nov_dim@mail.ru>
2022-06-18 11:37:47 +0300

    add multiplicationNumbers

--- classwork2/Program.cs
+++ classwork2/Program.cs
@@ -10,32 +10,60 @@ namespace ConsoleApp
             void SumNumbers (int number) //метод вывода кубов чисел от 1 до N.
             {
                 int sum = 0;
-                for (int i = 1; i <= number;  i++) // циклом проходимя по массивам pointOne, pointTwo чтобы получить сумму разностей точек в квадрате 
+                for (int i = 1; i <= number;  i++)
                 {
                     sum = sum + i;
                 }
-                Console.Write($"{sum}");
+                Console.Write($"{sum} \n");
                 
             }
-            Console.WriteLine("Введите число:");
-            int inputNumber = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("__________");
-            Console.WriteLine("Задача 26");
+            Console.WriteLine("Задача 24");
+
+            Console.WriteLine("Введите число:");
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
             SumNumbers(inputNumber);
 
             // Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
 
             void CountNumbers (string number) //метод вывода кубов чисел от 1 до N.
             {
-                char[] chars = number.ToCharArray();
-                Console.Write($"{chars.Length}");
+                Console.Write($"{number.Length} \n");
             }
+
+
+            Console.WriteLine("__________");
+            Console.WriteLine("Задача 26");
+
             Console.WriteLine("Введите число:");
             string inputNumberA = Console.ReadLine();
+
             CountNumbers(inputNumberA);
 
             
+            // Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+            void multiplicationNumbers (int number) 
+            {
+                int multiplication = 1;
+                for (int i = 1; i <= number;  i++) 
+                {
+                    multiplication = multiplication * i;
+                }
+                Console.Write($"{multiplication} \n");
+            }
+
+            Console.WriteLine("__________");
+            Console.WriteLine("Задача 28");
+
+            Console.WriteLine("Введите число:");
+            int inputNumberB = Convert.ToInt32(Console.ReadLine());
+            multiplicationNumbers(inputNumberB);
+
+
+
+
         }
     }
 }
