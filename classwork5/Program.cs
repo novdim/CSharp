b2c4a65c5a23a9739d0976b246b5eb63da8b40b2
«novdim» <nov_dim@mail.ru>
2022-06-26 11:31:42 +0300

    add  FillArray48, FillArray49, FillArray5

--- classwork5/Program.cs
+++ classwork5/Program.cs
@@ -6,14 +6,50 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            int size_rows = 4;
-            int size_colum = 3;
+            Random random = new Random();
+            int size_rows = 2;
+            int size_colum = 22;
             int start = 1;
             int end = 10;
-
             int[,] new_Array = RandFillArray(size_rows, size_colum, start, end);
             PrintArray(new_Array);
 
+
+            // Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: A = m+n.
+            // Выведите полученный массив на экран.
+
+            Console.WriteLine("____________");
+            Console.WriteLine("Задача 48: ");
+            FillArray48(new_Array);
+
+            // Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+            Console.WriteLine("____________");
+            Console.WriteLine("Задача 49: ");
+
+            FillArray49(new_Array);
+
+            //Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+            Console.WriteLine("____________");
+            Console.WriteLine("Задача 51: ");
+
+            FillArray51(new_Array);
+
+            // Дан двумерный массив из двух строк и двадцати двух столбцов. 
+            // В его первой строке записано количество мячей, забитых футбольной командой в той или иной игре, во второй — количество пропущенных мячей в этой же игре.
+            а) Для  каждой  проведенной  игры  напечатать  словесный  результат:  "выигрыш", "ничья" или "проигрыш". 
+            б) Определить количество выигрышей данной команды. 
+            в) Определить  количество  выигрышей  и  количество  проигрышей  данной   команды. 
+            г) Определить количество выигрышей, количество ничьих и количество проигрышей данной команды. 
+            д) Определить,  в  скольких  играх  разность  забитых  и  пропущенных  мячей  была большей или равной трем. 
+            е) Определить общее число очков, набранных командой (за выигрыш даются 3 очка, за ничью — 1, за проигрыш — 0).
+
+
+
+
+
+            // методы
             int[,] RandFillArray (int rows, int columns, int start, int end)
             {
                 Random random = new Random();
@@ -40,21 +76,8 @@ namespace ConsoleApp
                 }
             }
             
-            // Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: A = m+n.
-            // Выведите полученный массив на экран.
-
-
-            Console.WriteLine();
-            Console.WriteLine("____________");
-            Console.WriteLine("Задача 48: ");
-            int[,] new_array_1 = FillArray(size_rows, size_colum, start, end);
-            PrintArray(new_array_1);
-
-
-            int[,] FillArray (int rows, int columns, int start, int end)
+            void FillArray48 (int [,] array)
             {
-                Random random = new Random();
-                int[,] array = new int[rows,columns];
                 for(int i = 0; i < array.GetLength(0); i++)
                 {
                     for (int j = 0; j < array.GetLength(1); j++)
@@ -62,11 +85,37 @@ namespace ConsoleApp
                         array[i ,j] = i + j;
                     }                    
                 }
-                return array;
+                PrintArray(array);
             }
 
-            // Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+            void FillArray49 (int [,] array)
+            {
+                for(int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        if (i%2 == 0 && j%2 == 0) array[i,j] = array[i,j]*array[i,j];
+                    }                    
+                }
+                PrintArray(array);
 
+            }
+
+            void FillArray51 (int [,] array)
+            {
+                int sum = 0;
+                for(int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        if (i == j) sum = sum + array[i,j];
+                    }                    
+                }
+                Console.WriteLine($"Сумма элементов, находящихся на главной диагонали {sum}");
+            }
+
+
+            
         }
     }
 }
