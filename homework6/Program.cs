f355fa3ae4e35574ef5addc469f1851b79448abb
novdim <nov_dim@mail.ru>
2022-06-27 19:29:36 +0300

    add DecimalToBinaryNumber

--- homework6/Program.cs
+++ homework6/Program.cs
@@ -7,96 +7,110 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
 
-        // Задача 41: Пользователь вводит с клавиатуры M чисел. 
-        // Посчитайте, сколько чисел больше 0 ввёл пользователь.
+            // Задача 41: Пользователь вводит с клавиатуры M чисел. 
+            // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-        Console.WriteLine($"__________");
-        Console.WriteLine($"Задача 41:");
+            Console.WriteLine($"__________");
+            Console.WriteLine($"Задача 41:");
 
-        Console.WriteLine("Введите число");
-        int quantity_number = 3;
-        CountingNumbersMoreZero(quantity_number);
+            Console.WriteLine("Введите число");
+            int quantity_number = 3;
+            CountingNumbersMoreZero(quantity_number);
 
 
-        Console.WriteLine($"(версия 2):");
-        Console.WriteLine("Введите несколько чисел. Чтобы остановить ввод чисел введите q");
-        CountingNumbersMoreZeroVersion2();
-        
-        // Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, 
-        // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-        
-        Console.WriteLine($"__________");
-        Console.WriteLine($"Задача 43:");
+            Console.WriteLine($"(версия 2):");
+            Console.WriteLine("Введите несколько чисел. Чтобы остановить ввод чисел введите q");
+            CountingNumbersMoreZeroVersion2();
+            
+            // Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, 
+            // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+            
+            Console.WriteLine($"__________");
+            Console.WriteLine($"Задача 43:");
 
-        SearchPointIntersectionLines();
+            SearchPointIntersectionLines();
 
-        
-        // Методы
-        
+            
+            // Методы
+            
 
-        void CountingNumbersMoreZero (int quantity)
-        {
-            int count = 0;
-            int number = 0;
-            for(int i = 0; i < quantity; i++)
+            void CountingNumbersMoreZero (int quantity)
             {
-                number = Convert.ToInt32(Console.ReadLine());
-                if(number > 0) count++;
-            }
-            Console.WriteLine($"Вы ввели {count} числа(-ел) больше 0.");
-        }
-
-        void CountingNumbersMoreZeroVersion2 ()
-        {
-            int count = 0;
-            string number = string.Empty;
-            int last_number = 0;
-
-            while(true)
-            {
-                number = Console.ReadLine();
-                if(number == "q") break;
-                else last_number = Convert.ToInt32(number);
-                if(last_number > 0) count++;
-            }
-            Console.WriteLine($"Вы ввели {count} чисел(а) больше 0.");
-        }
-
-        void SearchPointIntersectionLines ()
-        {
-            double coordinate_x = 0, coordinate_y = 0, k1 = 0, k2 = 0, b1 = 0, b2 = 0;
-
-            while(true)
-            {
-                Console.WriteLine($" Введите k1 для уровнения y = k1 * x + b1");
-                k1 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine($" Введите b1 для уровнения y = k1 * x + b1");
-                b1 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine($" Введите k2 для уровнения y = k2 * x + b2;");
-                k2 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine($" Введите b2 для уровнения y = k2 * x + b2;");
-                b2 = Convert.ToInt32(Console.ReadLine());
-
-                if (k1 == k2 && b1 != b2)
+                int count = 0;
+                int number = 0;
+                for(int i = 0; i < quantity; i++)
                 {
-                    Console.WriteLine($"При таких показателях линии не будут переcекаться. Они будут параллельными. Повторите ввод!"); 
-                    continue;
+                    number = Convert.ToInt32(Console.ReadLine());
+                    if(number > 0) count++;
                 }
-                else if (k1 == k2 && b1 == b2)
-                {
-                    Console.WriteLine($"При таких показателях линии не будут переcекаться. Они совпадают. Повторите ввод!"); 
-                    continue;
-                }
-                else
-                {
-                    coordinate_x = (b2-b1)/(k1-k2);
-                    coordinate_y = k1 * coordinate_x + b1;
-                    Console.WriteLine($"Координаты точки пересечения(x,y) = ({coordinate_x},{coordinate_y})");
-                    break;
-                }
+                Console.WriteLine($"Вы ввели {count} числа(-ел, -о) больше 0.");
             }
 
-        }
+            void CountingNumbersMoreZeroVersion2 ()
+            {
+                int count = 0;
+                string number = string.Empty;
+                int last_number = 0;
+
+                while(true)
+                {
+                    number = Console.ReadLine();
+                    if(number == "q") break;
+                    else last_number = Convert.ToInt32(number);
+                    if(last_number > 0) count++;
+                }
+                Console.WriteLine($"Вы ввели {count} числа(-ел, -о) больше 0.");
+            }
+
+            void SearchPointIntersectionLines ()
+            {
+                double coordinate_x = 0, coordinate_y = 0, k1 = 0, k2 = 0, b1 = 0, b2 = 0;
+
+                while(true)
+                {
+                    Console.WriteLine($" Введите k1 для уровнения y = k1 * x + b1");
+                    k1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($" Введите b1 для уровнения y = k1 * x + b1");
+                    b1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($" Введите k2 для уровнения y = k2 * x + b2;");
+                    k2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($" Введите b2 для уровнения y = k2 * x + b2;");
+                    b2 = Convert.ToInt32(Console.ReadLine());
+
+                    if (k1 == k2 && b1 != b2)
+                    {
+                        Console.WriteLine($"При таких показателях линии не будут переcекаться. Они будут параллельными. Повторите ввод!"); 
+                        continue;
+                    }
+                    else if (k1 == k2 && b1 == b2)
+                    {
+                        Console.WriteLine($"При таких показателях линии не будут переcекаться. Они совпадают. Повторите ввод!"); 
+                        continue;
+                    }
+                    else
+                    {
+                        coordinate_x = (b2-b1)/(k1-k2);
+                        coordinate_y = k1 * coordinate_x + b1;
+                        Console.WriteLine($"Координаты точки пересечения(x,y) = ({coordinate_x}, {coordinate_y})");
+                        break;
+                    }
+                }
+
+            }
+
+            // Задача 1. Написать перевод десятичного числа в двоичное, используя рекурсию.
+
+            Console.WriteLine("Введите число:");
+            int input_number = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine($"Введенное число {input_number} в двоичной системе счисления {DecimalToBinaryNumber(input_number)} ");
+
+            string DecimalToBinaryNumber(int number)
+            {
+                if(number == 0) return "";
+                else return DecimalToBinaryNumber(number / 2) + (number % 2).ToString();
+            }
+            
         }
     }
 }
