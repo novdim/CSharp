d66537d5d2b2a861dbeee84ede94464db96d7304
novdim <nov_dim@mail.ru>
2022-06-28 21:13:03 +0300

    add MatrixMultiplication

--- homework7/Program.cs
+++ homework7/Program.cs
@@ -67,6 +67,26 @@ namespace ConsoleApp
             
             ArithmeticMeanColumn(last_array);
 
+            // Задача 1. Даны две матрицы, количество строк и столбцов которых может быть 3 или 4, 
+            // заполнены числами от -9 до 9. Выполните умножение матриц.
+
+
+            Console.WriteLine($" \n__________");
+            Console.WriteLine($"Доп. Задача 1:");
+
+
+
+            int[,] array_1 = RandArray(5, 5, -9, 10);
+            int[,] array_2 = RandArray(5, 5, -9, 10);
+            Console.WriteLine($"____1____");
+            PrintArray(array_1);
+            Console.WriteLine($"____2____");
+            PrintArray(array_2);
+            Console.WriteLine($"____Итог_____");
+            MatrixMultiplication(array_1, array_2);
+
+            
+
             // Методы
 
             double[,] RandDoubleArray(int rows, int columns, int start, int end)            // метод генерации массива вещественных     
@@ -86,11 +106,11 @@ namespace ConsoleApp
                 return array;
             }
 
-            int[,] RandArray(int max_rows, int max_columns, int start, int end)             // метод генерации массива целых чисел от 3 и более строк и от 4 и более столбцов     
+            int[,] RandArray(int max_rows, int max_columns, int start, int end)             // метод генерации массива целых чисел от 3 и более строк и от 3 и более столбцов     
             {
                 Random random = new Random();
                 int rows = random.Next(3,max_rows);
-                int columns = random.Next(4,max_columns);
+                int columns = random.Next(3,max_columns);
 
                 int[,] array = new int[rows, columns];
 
@@ -141,6 +161,7 @@ namespace ConsoleApp
             void ArithmeticMeanColumn(int [,] currentArray)                                 // метод среднего арифметического элементов столбца
             {
                 double arithmetic_mean = 0;
+                
                 double length_column = currentArray.GetLength(0);
                 double sum = 0;
                 for (int j = 0; j < currentArray.GetLength(1); j++)
@@ -154,6 +175,37 @@ namespace ConsoleApp
                     arithmetic_mean = sum / length_column;
                     Console.Write($"{Math.Round(arithmetic_mean, 1)} \t");
                 }
+                
+            }
+            
+
+            //___________________ методы для доп. задач___________________
+
+
+
+            void MatrixMultiplication(int [,] matrix_1, int [,] matrix_2)                                 // метод среднего арифметического элементов столбца
+            {
+              
+                int[,] result_matrix = new int[matrix_1.GetLength(0), matrix_2.GetLength(1)];
+
+                if (matrix_1.GetLength(1) != matrix_2.GetLength(0))
+                {
+                    Console.WriteLine("Нельзя перемножить матрицы");
+                }
+                else
+                {
+                    for (int i = 0; i < matrix_1.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < matrix_2.GetLength(1); j++)
+                        {
+                            for (int k = 0; k < matrix_2.GetLength(0); k++)
+                            {
+                                result_matrix[i, j] += matrix_1[i, k] * matrix_2[k, j];
+                            }
+                        }
+                    }
+                    PrintArray(result_matrix);
+                }      
             }
 
         }
