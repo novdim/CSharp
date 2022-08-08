59cef28f74d45a5858bac7f79c8c923ac5b33819
novdim <nov_dim@mail.ru>
2022-08-08 12:25:00 +0300

    add BubbleSorting

--- sorting_algorithms/Program.cs
+++ sorting_algorithms/Program.cs
@@ -1,7 +1,12 @@
-﻿// Алгоритм «Сортировка выбором»
-
+﻿
 int[] array = GeneratorArray(5, -10, 10);
+int[] array1 = GeneratorArray(5, -10, 10);
+Console.WriteLine("Сортировка выбором");
 PrintArray(ChoiceSorting(array));
+Console.WriteLine();
+Console.WriteLine("Сортировка пузырьком");
+PrintArray(BubbleSorting(array1));
+
 
 int[] GeneratorArray(int length, int min_number, int max_number)
 {
@@ -14,11 +19,20 @@ int[] GeneratorArray(int length, int min_number, int max_number)
     return array;
 }
 
+void PrintArray (int[] array)
+{
+    Console.WriteLine($"Отсортированный массив: [{string.Join(", ", array)}]");
+}
 
+
+// МЕТОДЫ Сортировки
+
+// Сортировка выбором
 
 int[] ChoiceSorting(int[] array)
 {
     Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+
     // Cортировка
     for (int i = 0; i < array.Length - 1; i++)
     {
@@ -35,7 +49,24 @@ int[] ChoiceSorting(int[] array)
     return array;      
 }
 
-void PrintArray (int[] array)
+// Cортировка пузырьком
+
+int[] BubbleSorting(int[] array)
 {
-    Console.WriteLine($"Отсортированный массив: [{string.Join(", ", array)}]");
+    Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+
+    // Cортировка
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array.Length - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;      
 }
